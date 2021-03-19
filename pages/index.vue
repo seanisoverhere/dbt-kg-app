8f5b99e49c539f4f2a486e18aca3246eb538860c
Li KangZheng <likzdev@gmail.com>
2021-03-19 18:16:25 +0800

    Added resident signup page

--- pages/index.vue
+++ pages/index.vue
@@ -1,79 +1,35 @@
 <template>
-  <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-      <div class="text-center">
-        <logo />
-        <vuetify-logo />
-      </div>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-          <p>
-            For more information on Vuetify, check out the <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              documentation
-            </a>.
-          </p>
-          <p>
-            If you have questions, please join the official <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="chat"
-            >
-              discord
-            </a>.
-          </p>
-          <p>
-            Find a bug? Report it on the github <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="contribute"
-            >
-              issue board
-            </a>.
-          </p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3">
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt Documentation
-          </a>
-          <br>
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn
-            color="primary"
-            nuxt
-            to="/inspire"
-          >
-            Continue
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-col>
-  </v-row>
+  <v-container>
+    <v-row>
+      <v-col cols="12">
+        <v-text-field
+          v-model="email"
+          label="Email"
+          outlined
+          clearable
+        ></v-text-field>
+      </v-col>
+      <v-col cols="12">
+        <v-text-field
+          v-model="address"
+          label="Address"
+          outlined
+          clearable
+          style="margin-top: -16px"
+        ></v-text-field>
+      </v-col>
+      <v-col cols="12">
+        <v-btn
+          @click="confirmationMessage = 'Thank you for using KGApp, you will be notified when a Karang Guni is available to collect your item'"
+          color="primary" block style="height: 40px; margin-top: -16px;">
+          Submit
+        </v-btn>
+      </v-col>
+      <v-col cols="12">
+        {{ confirmationMessage }}
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
@@ -84,6 +40,14 @@ export default {
   components: {
     Logo,
     VuetifyLogo
-  }
+  },
+  data() {
+    return {
+      email: '',
+      address: '',
+      confirmationMessage: '',
+    }
+  },
+  method: {}
 }
 </script>
